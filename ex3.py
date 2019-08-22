98bff5a5f2562abc071f1a21f82c1d7cd3a96095
hagios <hagioswilson@gmail.com>
2019-08-22 12:39:27 -0400

    modification in written files

--- ex3.py
+++ ex3.py
@@ -1,13 +1,17 @@
 from sys import argv
 
+from os.path import exists
+
 script, copy_from, copy_to = argv
 
 #copying contents from one file (sample.txt)
 content = open(copy_from).read()
 
 #now pasting the copied content in to another file (sample1.txt)
-newFile = open(copy_to, 'w').write("Copied Content\n" + content)
+open(copy_to, 'w').write(content)
 
-if newFile:
+fileContentLenght = len(open(copy_to).read())
 
+if exists(copy_to) and fileContentLenght == len(content):
+     
     print("Contents were successfully copied") 
